c12c8b0c8bf328b9a8238e2a6b19aba2241c3bd1
amruta12021995 <Admin@DESKTOP-UD9B62Q>
2024-02-14 11:23:45 +0530

    I am sending Logical Operators program....!!!

---
+++ src/operators/LogicalOperators.java
@@ -0,0 +1,16 @@
+package operators;
+
+public class LogicalOperators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		 boolean a = true, b = false;
+	        System.out.println("Logical AND: " + (a && b));
+	        System.out.println("Logical OR: " + (a || b));
+	        System.out.println("Logical NOT: " + !a);
+	    }
+	
+	}
+
+
