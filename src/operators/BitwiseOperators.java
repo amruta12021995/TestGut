d91bb0555b9c4c7dfd6995aec0f9cc80d66ef65c
amruta12021995 <Admin@DESKTOP-UD9B62Q>
2024-02-14 11:18:19 +0530

    only one class code data i am Sending here....!!!

---
+++ src/operators/BitwiseOperators.java
@@ -0,0 +1,20 @@
+package operators;
+
+public class BitwiseOperators {
+
+	public static void main(String[] args) {
+		int x = 5, y = 3;
+        System.out.println("Bitwise AND: " + (x & y));
+        System.out.println("Bitwise OR: " + (x | y));
+        System.out.println("Bitwise XOR: " + (x ^ y)); // how it operates
+
+	}
+
+}
+
+//Answer
+/*
+Bitwise AND: 1
+Bitwise OR: 7
+Bitwise XOR: 6
+*/
