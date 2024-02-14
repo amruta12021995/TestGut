a9d4d8e087df99f9d23d121b89c8771c31f16ed5
amruta12021995 <Admin@DESKTOP-UD9B62Q>
2024-02-14 11:26:45 +0530

    StringComparisonOperator programm

---
+++ src/operators/StringComparison.java
@@ -0,0 +1,12 @@
+package operators;
+
+public class StringComparison {
+
+	public static void main(String[] args) {
+		 String str1 = "Hello", str2 = "World";
+	        System.out.println("Concatenation: " + (str1 + " " + str2));
+	        System.out.println("Equality: " + str1.equals(str2));
+
+	}
+
+}
